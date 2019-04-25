844559f5838cc7d16ef9faf8c173b57a5f82f02b
Sergey Tovt <st@norse.co>
2019-04-25 13:17:05 +0300

    update dependencies

--- src/store/middleware/routerMiddleware.js
+++ src/store/middleware/routerMiddleware.js
@@ -1,5 +1,5 @@
 // @flow
-import createBrowserHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import config from 'config';
 
