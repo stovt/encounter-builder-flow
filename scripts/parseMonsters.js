26a122794485c7f4273121714a52135395543faa
Sergey Tovt <st@norse.co>
2019-05-31 12:31:24 +0300

    update "parse monsters" script

--- scripts/parseMonsters.js
+++ scripts/parseMonsters.js
@@ -7,10 +7,18 @@ const projectRoot = path.resolve(__dirname, '../');
 const publicRoot = path.resolve(projectRoot, 'public');
 const publicPath = file => path.resolve(publicRoot, file);
 
-const { REACT_APP_API_ENDPOINT } = process.env;
+const {
+  REACT_APP_API_ENDPOINT,
+  REACT_APP_CORS_ENDPOINT
+} = process.env;
 
 if (!REACT_APP_API_ENDPOINT) {
   console.error('You have to specify REACT_APP_API_ENDPOINT environment variable in order to run this script.');
+  process.exit(1);
+}
+
+if (!REACT_APP_CORS_ENDPOINT) {
+  console.error('You have to specify REACT_APP_CORS_ENDPOINT environment variable in order to run this script.');
   process.exit(1);
 }
 
@@ -23,7 +31,7 @@ const OPTIONS = {
 };
 
 const fetchMonsterByID = monsterID => (
-  fetch(`${REACT_APP_API_ENDPOINT}/monsters/${monsterID}.json`, OPTIONS)
+  fetch(`${REACT_APP_CORS_ENDPOINT}${REACT_APP_API_ENDPOINT}/monsters/${monsterID}`, OPTIONS)
     .then(result => result.json())
     .then((monster) => {
       fs.writeFile(publicPath(`data/monsters/${monsterID}.json`), JSON.stringify(monster), (err) => {
@@ -31,7 +39,7 @@ const fetchMonsterByID = monsterID => (
           console.log(err);
           console.error(`Error writing ${monsterID}.json file.`);
         } else {
-          console.log(`Monsters ${monster.name} successfully parsed!`);
+          console.log(`Monster ${monster.name} successfully parsed!`);
         }
       });
     })
@@ -41,21 +49,33 @@ const fetchMonsterByID = monsterID => (
     })
 );
 
-fetch(`${REACT_APP_API_ENDPOINT}/monster-index.json`, OPTIONS)
-  .then(result => result.json())
-  .then((monsters) => {
-    fs.writeFile(publicPath('data/monsters/monsters.json'), JSON.stringify(monsters), (err) => {
-      if (err) {
-        console.log(err);
-        console.error('Error writing monsters.json file.');
-      } else {
-        console.log('Monsters successfully parsed!');
-      }
-    });
+const getMonsters = (nextPageUrl = '', monsters = []) => {
+  const apiUrl = nextPageUrl ? `${REACT_APP_CORS_ENDPOINT}${nextPageUrl}` : `${REACT_APP_API_ENDPOINT}/monsters`;
 
-    monsters.forEach(monster => fetchMonsterByID(monster.slug));
-  })
-  .catch((err) => {
-    console.error(err);
-    console.error('Error requesting the endpoint.');
+  return fetch(apiUrl, OPTIONS)
+    .then(result => result.json())
+    .then((data) => {
+      const allMonsters = [...monsters, ...data.results];
+      if (data.next) {
+        return getMonsters(data.next, allMonsters);
+      }
+      return Promise.resolve(allMonsters);
+    })
+    .catch((err) => {
+      console.error(err);
+      console.error('Error requesting the endpoint.');
+    });
+};
+
+getMonsters().then((monsters) => {
+  fs.writeFile(publicPath('data/monsters/monsters.json'), JSON.stringify(monsters), (err) => {
+    if (err) {
+      console.log(err);
+      console.error('Error writing monsters.json file.');
+    } else {
+      console.log('Monsters successfully parsed!');
+    }
   });
+
+  monsters.forEach(monster => fetchMonsterByID(monster.slug));
+});
